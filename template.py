11a1004daf84084c55e94cfaca5341ddbce277e3
unknown <moonean200796@gmail.com>
2025-06-01 21:39:48 +0400

    exception, logger & utils added

--- template.py
+++ template.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 project_name = 'mlProject'
 
 list_of_files = [
-    ".github/workflowss/.gitkeep",
+    ".github/workflowss/.gitkeep", 
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
